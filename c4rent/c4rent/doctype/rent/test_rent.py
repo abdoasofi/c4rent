32845fe20c6ff6222217ec9478608634e398fee0
abdoasofi <asofi@bonya-ye.org>
2025-02-08 12:28:39 +0300

    Initial commit

---
+++ c4rent/c4rent/doctype/rent/test_rent.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Connect 4 Systems and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRent(FrappeTestCase):
+	pass
---
+++ c4rent/c4rent/doctype/rent_detail/rent_detail.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Connect 4 Systems and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class RentDetail(Document):
+	pass
