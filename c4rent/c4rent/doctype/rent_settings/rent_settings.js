249dea10b89af84cd62fc56bebfe64f3e436b496
abdoasofi <asofi@bonya-ye.org>
2025-02-17 00:53:48 +0300

    [Add] : Doctype Rent Settings.

---
+++ c4rent/c4rent/doctype/rent_settings/rent_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Connect 4 Systems and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Rent Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
